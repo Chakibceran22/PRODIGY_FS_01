1ccc429a2c3decf38f8665ef31a8fedd7d0afc95
Chakibceran22 <grabachakib008@gmail.com>
2025-03-02 18:46:56 +0100

    creating the sigup logic in the backend

---
+++ back/middleware/signup.js
@@ -0,0 +1,21 @@
+import User from '../models/user.js'
+export const createUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const newUser = new User({
+      username: username,
+      email: email,
+      password: password,
+    });
+
+    await newUser.save();
+    return res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
