e0b8fe4a9b86ce2dbe226f72e4188a8b7291ef36
Chakibceran22 <grabachakib008@gmail.com>
2025-03-02 18:11:08 +0100

    adding some rerouting stuff

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route,Routes, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import LandingPage from "../componenetsTest/LandingPage";
 const App = () => {
   return(
     <Router>
     <Routes>
+      <Route path="/" element={<Navigate to='/login' replace/>}/>
       <Route path="/login" element={<LoginPage/>}/>
     </Routes>
   </Router>
